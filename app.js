ebb8fe5cee7019d37b91445c672c5ffc4b20b493
Noelia Vaz <nlvazquez01@gmail.com>
2019-05-08 09:00:56 -0400

    update

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 const express = require('express');
+const app = express();
+const { projects } = require('./data.json');
 
-console.log("Hello world");
+app.use('/static', express.static('public'));
+
+app.set('view engine', 'pug');
+
+app.use('/', (req, res) => {
+  res.render('index', { projects });
+});
+//app.use('/about', );
+//app.use('/project', );
+
+
+
+app.listen(3000, () => {
+	console.log('The application is running on localhost:3000!');
+});
